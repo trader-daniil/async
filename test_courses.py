b8c0508c94a8dd632907447691785537a6955fdd
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-10-06 19:18:51 +0300

    add spaceship animation

--- test_courses.py
+++ test_courses.py
@@ -11,7 +11,7 @@ STARS = (
     '.',
     ':',
 )
-STARS_AMOUNT = 20
+STARS_AMOUNT = 300
 
 
 class EventLoopCommand():
@@ -44,12 +44,122 @@ async def blink(canvas, row, column, symbol='*'):
         await Sleep(0.3)
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
+
+def draw_frame(canvas, start_row, start_column, text, negative=False):
+    """Draw multiline text fragment on canvas, erase text instead of drawing if negative=True is specified."""
+    
+    rows_number, columns_number = canvas.getmaxyx()
+
+    for row, line in enumerate(text.splitlines(), round(start_row)):
+        if row < 0:
+            continue
+
+        if row >= rows_number:
+            break
+
+        for column, symbol in enumerate(line, round(start_column)):
+            if column < 0:
+                continue
+
+            if column >= columns_number:
+                break
+                
+            if symbol == ' ':
+                continue
+
+            if row == rows_number - 1 and column == columns_number - 1:
+                continue
+
+            symbol = symbol if not negative else ' '
+            canvas.addch(row, column, symbol)
+
+
+async def animate_spaceship(canvas, row, column, rocket_1, rocket_2):
+    """
+    Отрисовывает корабль.
+    """
+    while True:
+        draw_frame(
+            canvas=canvas,
+            start_row=row,
+            start_column=column,
+            text=rocket_1,
+        ) 
+        await Sleep(0.5)
+        draw_frame(
+            canvas=canvas,
+            start_row=row,
+            start_column=column,
+            text=rocket_1,
+            negative=True,
+        ) 
+        await Sleep(0.5)
+        draw_frame(
+            canvas=canvas,
+            start_row=row,
+            start_column=column,
+            text=rocket_2,
+        )
+        await Sleep(0.5)
+        draw_frame(
+            canvas=canvas,
+            start_row=row,
+            start_column=column,
+            text=rocket_2,
+            negative=True,
+        )
+        await Sleep(0.5)
+
+
 def draw(canvas):
     """
     Рисует получая данные от корутины.
-    """    
+    """
+      
     row, column = canvas.getmaxyx()
-
+    
+    with open('rocket_frame_1.txt') as f:
+        rocket_1 = f.read()
+    with open('rocket_frame_2.txt') as f:
+        rocket_2 = f.read()
+    spaceship = animate_spaceship(
+        canvas=canvas,
+        row=row//2,
+        column=column//2,
+        rocket_1=rocket_1,
+        rocket_2=rocket_2,
+    )
+    
     courutines = []
     for _ in range(STARS_AMOUNT):
         courutines.append(
@@ -60,21 +170,29 @@ def draw(canvas):
                 symbol=random.choice(STARS),    
             ),
         )
-    for courutine in courutines:
-        courutine.send(None)
-    canvas.border()
-    curses.curs_set(False)
-    canvas.refresh()
+    shot = fire(canvas, start_row=row//2, start_column=column//2, rows_speed=-0.3, columns_speed=0)
+    #анимация выстрела
+    while True:
+        try:
+            shot.send(None)
+            canvas.border()
+            curses.curs_set(False)
+            canvas.refresh()
+            time.sleep(0.05)
+        except StopIteration:
+            break
+    #анимация звездного неба
     star_queues = list(chunked(courutines, 10))
     while True:
         for queue in star_queues:
             for courutine in queue:
                 courutine.send(None)
-            canvas.border()
-            curses.curs_set(False)
+            spaceship.send(None)
             canvas.refresh()
             time.sleep(0.1)
-
+            spaceship.send(None)
+        
 
 curses.update_lines_cols()
 curses.wrapper(draw)
+
